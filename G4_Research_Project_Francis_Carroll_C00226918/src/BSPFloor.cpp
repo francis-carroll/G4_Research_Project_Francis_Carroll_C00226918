8cf7281b9cdd3e1f30ad48bf9525e92247c5cd3b
francis-carroll <fcarroll29@gmail.com>
2020-12-08 08:38:23 +0000

    Random position of room in bsp node

--- G4_Research_Project_Francis_Carroll_C00226918/src/BSPFloor.cpp
+++ G4_Research_Project_Francis_Carroll_C00226918/src/BSPFloor.cpp
@@ -66,6 +66,8 @@ void BSPFloor::setupRooms()
 	for (shared_ptr<BSPNode> node : *leafNodes)
 	{
 		Vector2f size = Vector2f(BSPTree::randomFloat(MIN_ROOM_SIZE.x, node->getNodeData()->getSize().x), BSPTree::randomFloat(MIN_ROOM_SIZE.y, node->getNodeData()->getSize().y));
-		m_rooms.push_back(make_shared<Room>(node->getIdentifier(), node->getNodeData()->getPosition(), size, Vector2f(0.0f,0.f)));
+		Vector2f position = Vector2f(BSPTree::randomFloat(node->getNodeData()->getPosition().x, node->getNodeData()->getPosition().x + (node->getNodeData()->getSize().x - size.x)),
+									 BSPTree::randomFloat(node->getNodeData()->getPosition().y, node->getNodeData()->getPosition().y + (node->getNodeData()->getSize().y - size.y)));
+		m_rooms.push_back(make_shared<Room>(node->getIdentifier(), position, size, Vector2f(0.0f,0.f)));
 	}
 }
